c01018014a5cda5ffbac119f844b32a48060b6cb
Pontus <ponha098@localhost>
2015-12-03 16:25:26 +0100

    ger upp for idag, ingen kollision

--- src/block.h
+++ src/block.h
@@ -1,12 +1,16 @@
 #ifndef BLOCK_h
 #define BLOCK_h 
 
+
+
 class Block 
 {
 public: 
-	Block(); 
+	Block();
+	void add_house(Game_object*);
 	int number_of_houses(); 
-	bool city_destroyed();  
+	bool city_destroyed();
+	Point get_random_target();
 
 private: 
 
